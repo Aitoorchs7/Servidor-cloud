37a5bc67418425450a0665b6168a85afe2231633
DanielMartinCordero <danikai1902@gmail.com>
2026-04-07 10:00:44 +0200

    Version Día 1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         CloudServer cloudServer = new CloudServer();
         FogNode fogNode = new FogNode(cloudServer);
         EdgeNode edgeNode = new EdgeNode(fogNode);
 
         System.out.println("========Simulación Cloud - Fog - Edge ========\n");
 
-        edgeNode.sendData(5);
+        System.out.println("Escriba el número de datos que quiere registrar");
+        int numData = sc.nextInt();
+        edgeNode.sendData(numData);
+        System.out.println(fogNode.getAlertCount());
 
         cloudServer.showData();
         System.out.println("Alertas detectadas en FOG: "+ fogNode.getAlertCount());
