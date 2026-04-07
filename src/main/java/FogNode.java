a63284bad5cba9024dfc47feb7b63d5745d77580
ACHS7 <chicanosellesaitor@gmail.com>
2026-04-07 10:21:16 +0200

    Servidor Claude: Version 1 | Dia 7/4

---
+++ src/main/java/FogNode.java
@@ -0,0 +1,32 @@
+package main.java;
+
+public class FogNode {
+    private final CloudServer cloudServer;
+    private int alertCount;
+
+    public FogNode(CloudServer cloudServer) {
+        this.cloudServer = cloudServer;
+    }
+    public void processData(SensorData data){
+        System.out.println("[FOG] Dato recibido: "+data);
+        if(alertCount <20) {
+            if (data.getTemperature() > 30) {
+                alertCount++;
+
+                System.out.println("[FOG] Alerta: temperatura alta");
+            } else {
+                System.out.println("[FOG] Temperatura normal");
+            }
+            cloudServer.saveData(data);
+        }
+        else{
+            System.out.println("[FOG] Límite de alertas alcanzado");
+        }
+    }
+    public int getAlertCount() {
+        return alertCount;
+    }
+    public void setAlertCount(int alertCount) {
+        this.alertCount = alertCount;
+    }
+}
